f89c371e3cc3052e43fe0a82d74cb2d423d97015
Edson Dota <edsondota@gmail.com>
2018-08-18 23:14:54 -0300

    Init asp net core app for api
    - Add .gitignore
    - Remove unnecessary files

---
+++ knight-moves-api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using knight_moves_api.Models;
+
+namespace knight_moves_api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json("Hello World!");
+        }
+    }
+}
---
+++ knight-moves-api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace knight_moves_api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
