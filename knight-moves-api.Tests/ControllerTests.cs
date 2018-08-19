f22a1489a6669c9d3b16c3da1f45f7e8d5808cf0
Edson Dota <edsondota@gmail.com>
2018-08-19 11:38:54 -0300

    Test knight creation and possibles moves

--- knight-moves-api.Tests/ControllerTests.cs
+++ knight-moves-api.Tests/ControllerTests.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using knight_moves_api.Controllers;
+using knight_moves_api.Models;
+using knight_moves_api.Services;
 
 namespace knight_moves_api.Tests
 {
@@ -11,7 +14,8 @@ namespace knight_moves_api.Tests
         public void VerifyIndexViewType()
         {
             // Arrange
-            var controller = new HomeController();
+            var service = new MoveService();
+            var controller = new HomeController(service);
 
             // Act
             var result = controller.Index();
@@ -24,13 +28,58 @@ namespace knight_moves_api.Tests
         public void VerifyIndexViewContent()
         {
             // Arrange
-            var controller = new HomeController();
+            var service = new MoveService();
+            var controller = new HomeController(service);
 
             // Act
             var result = Assert.IsType<JsonResult>(controller.Index());
 
             // Assert
             Assert.Equal("Hello World!", result.Value.ToString());
+        }
+
+        [Fact]
+        public void VerifyKnightItsCreated()
+        {
+            var service = new MoveService();
+            var controller = new HomeController(service);
+            var result = Assert.IsType<JsonResult>(controller.Knight("d", 4));
+            Assert.IsType<Knight>(result.Value);
+        }
+
+        [Fact]
+        public void VerifyKnightHasPossiblesMoves()
+        {
+            var service = new MoveService();
+            var controller = new HomeController(service);
+            var result = Assert.IsType<JsonResult>(controller.Knight("d", 4));
+            var model = Assert.IsType<Knight>(result.Value);
+            Assert.IsType<List<Moves>>(model.PossibleMoves);
+        }
+
+        [Fact]
+        public void VerifyKnightHasEightMoves()
+        {
+            var coordx = 4;
+            var coordy = "d";
+            var service = new MoveService();
+            var controller = new HomeController(service);
+            var result = Assert.IsType<JsonResult>(controller.Knight(coordy, coordx));
+            var model = Assert.IsType<Knight>(result.Value);
+            var possibleMoves = Assert.IsType<List<Moves>>(model.PossibleMoves);
+            Assert.Equal(8, possibleMoves.Count);
+        }
+
+        public void VerifyKnightHasTwoMoves()
+        {
+            var coordx = 1;
+            var coordy = "h";
+            var service = new MoveService();
+            var controller = new HomeController(service);
+            var result = Assert.IsType<JsonResult>(controller.Knight(coordy, coordx));
+            var model = Assert.IsType<Knight>(result.Value);
+            var possibleMoves = Assert.IsType<List<Moves>>(model.PossibleMoves);
+            Assert.Equal(8, possibleMoves.Count);
         }
     }
 }
